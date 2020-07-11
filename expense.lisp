9f9bb5fc77c06e22186ce6312ef77442a26e30fc
Calandrinon <georgesut@yahoo.com>
2020-07-11 17:09:03 +0300

    Create equality checker for expenses

--- expense.lisp
+++ expense.lisp
@@ -5,3 +5,13 @@
 	 (price
 	  :accessor price
 	  :initarg :price)))
+
+
+(defmethod equal-expenses ((expense1 Expense) (expense2 Expense))
+	(if (and
+			(string-equal (expense-title expense1) (expense-title expense2))
+			(= (price expense1) (price expense2)))
+		t
+		nil	
+	)) 
+
